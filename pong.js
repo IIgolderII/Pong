52c152864eed1413d3d12294640e79f2da5569da
Antoine FAVEREAU <antoine.favereau@viacesi.fr>
2022-08-11 00:37:52 +0200

    wip

--- pong.js
+++ pong.js
@@ -55,7 +55,7 @@ class Balle {
             }
 
             if (batonGauche.mode == 'rouge') {
-                this.vitesse += .2;
+                this.vitesse = this.vitesseDefaut + .2;
             } else {
                 this.vitesse = this.vitesseDefaut;
             }
@@ -73,7 +73,7 @@ class Balle {
             }
 
             if (batonDroite.mode == 'rouge') {
-                this.vitesse += .2;
+                this.vitesse = this.vitesseDefaut + .2;
             } else {
                 this.vitesse = this.vitesseDefaut;
             }
